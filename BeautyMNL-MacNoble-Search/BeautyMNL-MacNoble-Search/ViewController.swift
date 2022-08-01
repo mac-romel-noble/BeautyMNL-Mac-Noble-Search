56e7c0187be2523d60f5c6dfe3ef74d33cf86ccf
Mac Romel D. Noble <>
2022-08-01 15:53:23 +0800

    Project init

---
+++ BeautyMNL-MacNoble-Search/BeautyMNL-MacNoble-Search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeautyMNL-MacNoble-Search
+//
+//  Created by Mac Romel D. Noble on 8/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
